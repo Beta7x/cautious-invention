ed0fca0245655b97181d933efc91ed63c5b9b919
Beta7x <adepriyantowidies@gmail.com>
2024-07-02 20:47:39 +0700

    [MODIFIED] - Change the return views at KriteriaController

--- app/Http/Controllers/KriteriaController.php
+++ app/Http/Controllers/KriteriaController.php
@@ -11,7 +11,7 @@ class KriteriaController extends Controller
     public function index()
     {
         $kriterias = Kriteria::all();
-        return view('Kriteria.index', compact('kriterias'));
+        return view('kriteria.index', compact('kriterias'));
     }
 
     public function create()
